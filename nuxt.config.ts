d6b9b15a5192f830b746bd0539a54020a3320935
WDN <78674458+wduanni@users.noreply.github.com>
2025-11-23 20:39:51 +0800

    Set CORS to true for API routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,7 @@ export default defineNuxtConfig({
       redirect: '/dashboard/links',
     },
     '/api/**': {
-      cors: process.env.NUXT_API_CORS === 'true',
+      cors: true,
     },
   },
   experimental: {
